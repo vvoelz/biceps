aecc10ba0156ad07fabb1168076ad4954c4ac098
yunhuige <yunhui.ge@gmail.com>
2017-01-30 11:30:36 -0500

    updated for TER and HETATM terms

--- scripts/cleanpdb.py
+++ scripts/cleanpdb.py
@@ -8,9 +8,9 @@ lines=f.readlines()
 f.close()
 pdb=[]
 for i in range(len(lines)):
-	if lines[i][:5] == 'ATOM ':
+	if lines[i][:5] == 'ATOM ' or lines[i][:6] == 'HETATM' or lines[i][:3] == 'TER':
 		pdb.append(lines[i].strip())
-outfile='clean.pdb'
+outfile='new.pdb'
 fid=open(outfile,'w')
 for i in pdb:
 	fid.writelines(i+'\n')
