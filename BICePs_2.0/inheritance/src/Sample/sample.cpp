7ee9389d401f1961e2565f6a3efc29988168b138
Rob Raddi <rraddi@temple.edu>
2018-07-16 15:04:57 -0400

    added some notes

--- BICePs_2.0/inheritance/src/Sample/sample.cpp
+++ BICePs_2.0/inheritance/src/Sample/sample.cpp
@@ -4,11 +4,38 @@
 
 using namespace std;
 
+/* The parameters that need to be placed as elements inside the array
+ * sigma_noe,sigma_J,sigma_cs_H,sigma_cs_Ha,sigma_cs_Ca,sigma_cs_N,sigma_pf
+ * need indices for all above
+ * gamma,gmma_index
+ * sse
+ *
+ */
+
+
+/* Need to create a pointer for int step
+ * Create a pointer for the array length
+ *
+ *
+ */
+
+//float *Para;
+//Para = &(Number of parameters - ?length of self.restraints?)
+
+///* Need to know how many parameters to sample over */
+//float array[&Para];  // array to hold parameters
+//
+// nsteps  = &steps  // set step to point at the input argument for nsteps
+
+
+// array[step-1]
+
+
 //int sample(self,nsteps) {
 int sample(int nsteps) {
 	/* Perform nsteps of posterior sampling. */
 
-	for( int step = 0; step < nsteps; step = step + 1) {
+	for( int step = 0; step < nsteps; step++) {
 		float x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 		if (x < 0.16) {
 			std::cout << x << " < 0.16" << std::endl;
