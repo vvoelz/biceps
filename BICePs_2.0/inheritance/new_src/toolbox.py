759185ffba5a9f634a89c42590730338cb4346c5
Yunhui Ge <yunhui.ge@gmail.com>
2018-09-05 12:08:51 -0400

    new function added

--- BICePs_2.0/inheritance/new_src/toolbox.py
+++ BICePs_2.0/inheritance/new_src/toolbox.py
@@ -68,6 +68,32 @@ def sort_data(dataFiles):
     return data
 
 
+def list_res(input_data):
+    """Determine what scheme is included in sampling"""
+
+#    input_data = sort_data(data)
+    scheme=[]
+    for i in input_data[0]:
+            if i.endswith('.cs_H'):
+                scheme.append('cs_H')
+            elif i.endswith('.cs_Ha'):
+                scheme.append('cs_Ha')
+            elif i.endswith('.cs_N'):
+                scheme.append('cs_N')
+            elif i.endswith('.cs_Ca'):
+                scheme.append('cs_Ca')
+            elif i.endswith('.J'):
+                scheme.append('J')
+            elif i.endswith('.pf'):
+                scheme.append('pf')
+            elif i.endswith('.noe'):
+                scheme.append('noe')
+                scheme.append('gamma')
+            else:
+                raise ValueError("Incompatible File extension. Use:{*.noe, *.J, *.cs_H, *.cs_Ha, *.cs_N, *.cs_Ca, *.pf}")
+
+    return scheme
+
 def write_results(self, outfilename):
     """Writes a compact file of several arrays into binary format."""
 
@@ -79,3 +105,20 @@ def read_results(filename):
     loaded = np.load(filename)
     print loaded.items()
 
+def convert_pop_to_energy(pop_filename, out_filename=None):
+    """Convert population to energy for each state using U = -np.log(P)"""
+    if pop_filename.endwith('txt') or pop_filename.endwith('dat'):
+        pop = np.loadtxt(pop_filename)
+    elif pop_filename.endwith('npy'):
+        pop = np.load(pop_filename)
+    else:
+        raise ValueError('Incompatible file extention. Use:{.txt,.dat,.npy}')
+    energy=[]
+    # replace NaN in the list with a very small number
+    pop[np.isnan(pop)]=0.001
+    for i in pop:
+        energy.append(-np.log((i/float(sum(pop)))))
+    if out_filename == None:
+        np.savetxt('energy.txt',energy)
+    else:
+        np.savetxt(out_filename,energy)
