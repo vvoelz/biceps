d6f67ddbbd0b54a6bd6b27dff71686a74ec81328
Yunhui Ge <yunhui.ge@gmail.com>
2018-09-12 16:35:46 -0400

    another bug fixed in logZ function

--- BICePs_2.0/inheritance/new_src/PosteriorSampler.py
+++ BICePs_2.0/inheritance/new_src/PosteriorSampler.py
@@ -95,7 +95,7 @@ class PosteriorSampler(object):
         Z = 0.0
 #        for rest_index in range(len(self.ensemble[0])):
 #            for s in self.ensemble[rest_index]:
-        for s in self.ensemble[0]:
+        for s in self.ensemble:
             Z +=  np.exp(-s.free_energy)
         self.logZ = np.log(Z)
         self.ln2pi = np.log(2.0*np.pi)
