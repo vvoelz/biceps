0c42c4cdb275600f673f456ada49d3db8f677d88
Yunhui Ge <yunhui.ge@gmail.com>
2018-09-14 13:35:45 -0400

    new function added for computing J couplings of amino acids and small molecules

--- BICePs_2.0/inheritance/new_src/toolbox.py
+++ BICePs_2.0/inheritance/new_src/toolbox.py
@@ -15,6 +15,8 @@ import sys, os, glob
 import numpy as np
 import re
 import yaml, io
+from J_coupling import * # MDTraj altered src code
+from KarplusRelation import *
 
 ##############################################################################
 # Code
@@ -122,3 +124,76 @@ def convert_pop_to_energy(pop_filename, out_filename=None):
         np.savetxt('energy.txt',energy)
     else:
         np.savetxt(out_filename,energy)
+
+def get_J3_HN_HA(traj, top, frame=None,  model="Habeck", outname = None):
+    '''Compute J3_HN_HA for frames in a trajectories.
+    Parameters
+    ----------
+    traj: trajectory file
+    top: topology file
+    frame: specific frame for computing
+    model: Karplus coefficient models ["Ruterjans1999","Bax2007","Bax1997","Habeck" ,"Vuister","Pardi"]
+    outname: if not None, the output will be saved and a file name (in the format of string) is required.
+    '''
+    J=[]
+    if frame is None:
+            t = md.load(traj,top=top)
+            J = compute_J3_HN_HA(t, model = model)
+    if frame is not None:
+            for i in range(len(frame)):
+                t = md.load(traj,top=top)[frame[i]]
+                d = compute_J3_HN_HA(t, model = model)
+                if i == 0:
+                        J.append(d[0])
+                        J.append(d[1])
+                else:
+                        J.append(d[1])
+    if outname is not None:
+            print('saving output file...')
+            np.save(outname, J)
+            print('Done!')
+
+    return J
+
+def dihedral_angle(self, x0, x1, x2, x3):
+    """Calculate the signed dihedral angle between 4 positions.  Result is in degrees."""
+    #Calculate Bond Vectors b1, b2, b3
+    b1=x1-x0
+    b2=x2-x1
+    b3=x3-x2
+
+    #Calculate Normal Vectors c1,c2.  This numbering scheme is idiotic, so care.
+    c1=np.cross(b2,b3)
+    c2=np.cross(b1,b2)
+
+    Arg1=np.dot(b1,c1)
+    Arg1*=np.linalg.norm(b2)
+    Arg2=np.dot(c2,c1)
+    phi=np.arctan2(Arg1,Arg2)
+
+    # return the angle in degrees
+    phi*=180./np.pi
+    return(phi)
+
+def compute_nonaa_Jcoupling(traj, top=None, index, karplus_key)
+
+    if len(karplus_key) != len(index):
+        raise ValueError("The number of index must equale the number of karplus_key.")
+    if traj.endwith('.gro'):
+        conf = md.load(traj)
+    elif traj.endwith('.pdb'):
+        conf = md.load(traj)
+    else:
+        if top == None:
+            raise TypeError("To load a trajectory file, a topology file must be provided.")
+        conf = md.load(traj,top=top)
+    J = np.zeros((len(conf),len(index)))
+    karplus = KarplusRelation()
+    for i in range(len(J)):
+        for j in range(len(index)):
+            ri, rj, rk, rl = [conf.xyz[0,x,:] for x in index[j]]
+            model_angle = dihedral_angle(ri, rj, rk, rl)
+            J[i,j] = karplus.J(model_angle, karplus_key[j])
+    return J
+
+
