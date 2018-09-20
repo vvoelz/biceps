06cc19b94a4ccb32cb57142b95db12fce0d31117
Yunhui Ge <yunhui.ge@gmail.com>
2018-09-20 12:18:12 -0400

    gamma range added

--- BICePs_2.0/inheritance/new_src/init_res.py
+++ BICePs_2.0/inheritance/new_src/init_res.py
@@ -7,7 +7,7 @@ from Restraint import *
 
 
 
-def init_res(PDB_filename, lam, energy, ref, data, uncern):
+def init_res(PDB_filename, lam, energy, ref, data, uncern, gamma):
 #        Restraint.__init__(self, PDB_filename, ref, use_global_ref_sigma=True)
         if not isinstance(ref, basestring):
             raise ValueError("reference potential type must be a 'str'")
@@ -22,42 +22,40 @@ def init_res(PDB_filename, lam, energy, ref, data, uncern):
                 raise ValueError("uncertainty should be a list of three items: sigma_min, sigma_max, dsigma")
             else:
                 sigma_min, sigma_max, dsigma = uncern[0], uncern[1], uncern[2]
+        if not gamma: # if it is an empty list
+            gamma_min, gamma_max, dgamma = 0.05, 20.0, np.log(1.02)
+        else:
+            gamma_min, gamma_max, dgamma = gamma[0], gamma[1], gamma[2]
+
         if data!= None:
             if data.endswith('cs_H'):
                 R = Restraint_cs_H(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
                 
 
             elif data.endswith('cs_CA'):
                 R = Restraint_cs_Ca(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
 
             elif data.endswith('cs_Ha'):
                 R = Restraint_cs_Ha(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
 
             elif data.endswith('cs_N'):
                 R = Restraint_cs_N(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
 
             elif data.endswith('J'):
                 R = Restraint_J(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)  # good ref
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
 
             elif data.endswith('noe'):
                 R = Restraint_noe(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)   # good ref
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data, dloggamma = dgamma, gamma_min = gamma_min, gamma_max = gamma_max)
 
             elif data.endswith('pf'):
                 R = Restraint_pf(PDB_filename,ref=ref,dlogsigma=dsigma, sigma_min=sigma_min,sigma_max=sigma_max)
-                R.prep_observable(lam=lam, free_energy=energy,
-                        filename=data)
+                R.prep_observable(lam=lam, free_energy=energy, filename=data)
         else:
             raise ValueError("Incompatible File extension. Use:{.noe,.J,.cs_H,.cs_Ha, .cs_Ca, .cs_N,.pf}")
         return R
