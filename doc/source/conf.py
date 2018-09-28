ccf3686034eef46a7d0261fa9d6363de0e08bf37
Rob Raddi <rraddi@temple.edu>
2018-09-28 14:35:02 -0400

    npsphinx for Jinja2...

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,6 +40,59 @@ nbsphinx_kernel_name = 'python2'
 # If True, the build process is continued even if an exception occurs:
 nbsphinx_allow_errors = True
 
+# List of arguments to be passed to the kernel that executes the notebooks:
+#nbsphinx_execute_arguments = ['--InlineBackend.figure_formats={"png", "pdf"}']
+
+# Controls when a cell will time out (defaults to 30; use -1 for no timeout):
+#nbsphinx_timeout = 60
+
+# Default Pygments lexer for syntax highlighting in code cells:
+nbsphinx_codecell_lexer = 'ipython2'
+
+# Width of input/output prompts used in CSS:
+#nbsphinx_prompt_width = '8ex'
+
+# If window is narrower than this, input/output prompts are on separate lines:
+#nbsphinx_responsive_width = '700px'
+
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = env.doc2path(env.docname, base='doc') %}
+
+.. only:: html
+
+    .. role:: raw-html(raw)
+        :format: html
+
+    .. nbinfo::
+
+        This page was generated from `{{ docname }}`__.
+        Interactive online version:
+        :raw-html:`<a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>`
+
+    __ https://github.com/spatialaudio/nbsphinx/blob/
+        {{ env.config.release }}/{{ docname }}
+
+.. raw:: latex
+
+    \vfil\penalty-1\vfilneg
+    \vspace{\baselineskip}
+    \textcolor{gray}{The following section was generated from
+    \texttt{\strut{}{{ docname }}}\\[-0.5\baselineskip]
+    \noindent\rule{\textwidth}{0.4pt}}
+    \vspace{-2\baselineskip}
+"""
+
+# This is processed by Jinja2 and inserted after each notebook
+nbsphinx_epilog = r"""
+.. raw:: latex
+
+    \textcolor{gray}{\noindent\rule{\textwidth}{0.4pt}\\
+    \hbox{}\hfill End of
+    \texttt{\strut{}{{ env.doc2path(env.docname, base='doc') }}}}
+    \vfil\penalty-1\vfilneg
+"""
+
 
 
 
