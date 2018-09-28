3ac270804efea752295480b681e102b9787c4c56
Rob Raddi <rraddi@temple.edu>
2018-09-28 14:56:03 -0400

    comment out sys.path.insert

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@
 
 import os,sys
 #sys.path.insert(0, os.path.abspath('sphinxext'))
-sys.path.insert(0,'biceps/')
+#sys.path.insert(0,'biceps/')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
