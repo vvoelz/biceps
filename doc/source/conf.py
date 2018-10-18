7f61dbdb5a4f61bf314763e75a91bf0e9c5888f7
Rob Raddi <rraddi@temple.edu>
2018-10-18 15:45:12 -0400

    .

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,17 +31,26 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram',
     'sphinx.ext.autosummary',
+    'autoapi.sphinx',
     'autoapi.extension'
 ]
 
     #,    excluding this because of known error
+
+autoapi_modules = {
+        'biceps': {
+            'override': False,
+            'output': 'auto'
+   }
+}
+
 
 ## Document Python Code
 autoapi_type = 'python'
 #autoapi_root = '../../BICePs_2.0/inheritance/new_src' # Where HTML is generated
 autoapi_root = 'examples/albo/new_src/' # Where HTML is generated
 #autoapi_dirs = ['../','../../BICePs_2.0/inheritance/new_src'] # Directory of YAML sources
-autoapi_dirs = ['examples/albo/new_src/'] # Directory of YAML sources
+autoapi_dirs = ['./']#,'examples/albo/new_src/'] # Directory of YAML sources
 
 
 autodoc_default_flags = ['members', 'inherited-members']
