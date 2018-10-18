6194bc2ecc133a74a9437dbaf9d88add7281b4c7
Rob Raddi <rraddi@temple.edu>
2018-10-18 14:34:42 -0400

    fixing some readthedocbugs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,8 +38,11 @@ extensions = [
 
 ## Document Python Code
 autoapi_type = 'python'
-autoapi_root = '../../BICePs_2.0/inheritance/new_src' # Where HTML is generated
-autoapi_dirs = ['../','../../BICePs_2.0/inheritance/new_src'] # Directory of YAML sources
+#autoapi_root = '../../BICePs_2.0/inheritance/new_src' # Where HTML is generated
+autoapi_root = '../../doc/source/examples/albo/new_src/' # Where HTML is generated
+#autoapi_dirs = ['../','../../BICePs_2.0/inheritance/new_src'] # Directory of YAML sources
+autoapi_dirs = ['../','../../doc/source/examples/albo/new_src/'] # Directory of YAML sources
+
 
 autodoc_default_flags = ['members', 'inherited-members']
 numpydoc_class_members_toctree = False
