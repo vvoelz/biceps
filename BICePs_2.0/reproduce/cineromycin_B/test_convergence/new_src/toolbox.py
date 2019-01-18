64e1c4cb232bb8002ee0c1eeda1ee8e7b78cb429
yunhuige <yunhui.ge@gmail.com>
2019-01-18 13:08:47 -0500

    compute JSD function added in toolbox

--- BICePs_2.0/reproduce/cineromycin_B/test_convergence/new_src/toolbox.py
+++ BICePs_2.0/reproduce/cineromycin_B/test_convergence/new_src/toolbox.py
@@ -197,4 +197,82 @@ def compute_nonaa_Jcoupling(traj, index, karplus_key, top=None):
             J[i,j] = karplus.J(model_angle, karplus_key[j])
     return J
 
+def compute_JSD(T1,T2,T_total,traj):
+    '''Compute JSD for a given part of trajectory.
+    Parameters
+    ----------
+    T1, T2, T_total: part 1, part2 and total (part1 + part2)
+    traj: trajectory from BICePs sampling
+    '''
+    a = np.load(traj)['arr_0'].item()
+    rest = a['rest_type']
+    rest_type=[]
+    for i in rest:
+        if i.split('_')[1] != 'noe':
+            rest_type.append(i.split('_')[1])
+        elif i.split('_')[1] == 'noe':
+            rest_type.append(i.split('_')[1])
+            rest_type.append('gamma')
+    all_JSD = np.zeros(len(rest_type))
+    if 'gamma' in rest_type:
+        for i in range(len(rest_type)):
+            if i == len(rest_type)-1:   # means it is gamma
+                r1, r2, r_total = np.zeros(len(a['allowed_gamma'])),np.zeros(len(a['allowed_gamma'])),np.zeros(len(a['allowed_gamma']))
+                for j in T1:
+                    r1[j[4:][0][i-1][1]]+=1
+                for j in T2:
+                    r2[j[4:][0][i-1][1]]+=1
+                for j in T_total:
+                    r_total[j[4:][0][i-1][1]]+=1
+                N1=sum(r1)
+                N2=sum(r2)
+                N_total = sum(r_total)
+                H1 = -1.*r1/N1*np.log(r1/N1)
+                H1 = sum(np.nan_to_num(H1))
+                H2 = -1.*r2/N2*np.log(r2/N2)
+                H2 = sum(np.nan_to_num(H2))
+                H = -1.*r_total/N_total*np.log(r_total/N_total)
+                H = sum(np.nan_to_num(H))
+                JSD = H-(N1/N_total)*H1-(N2/N_total)*H2
+                all_JSD[i] = JSD
+            else:
+                r1, r2, r_total = np.zeros(len(a['allowed_sigma'][i])),np.zeros(len(a['allowed_sigma'][i])),np.zeros(len(a['allowed_sigma'][i]))
+                for j in T1:
+                    r1[j[4:][0][i][0]]+=1
+                for j in T2:
+                    r2[j[4:][0][i][0]]+=1
+                for j in T_total:
+                    r_total[j[4:][0][i][0]]+=1
+                N1=sum(r1)
+                N2=sum(r2)
+                N_total = sum(r_total)
+                H1 = -1.*r1/N1*np.log(r1/N1)
+                H1 = sum(np.nan_to_num(H1))
+                H2 = -1.*r2/N2*np.log(r2/N2)
+                H2 = sum(np.nan_to_num(H2))
+                H = -1.*r_total/N_total*np.log(r_total/N_total)
+                H = sum(np.nan_to_num(H))
+                JSD = H-(N1/N_total)*H1-(N2/N_total)*H2
+                all_JSD[i] = JSD
+    else:
+        for i in range(len(rest_type)):
+                r1, r2, r_total = np.zeros(len(a['allowed_sigma'][i])),np.zeros(len(a['allowed_sigma'][i])),np.zeros(len(a['allowed_sigma'][i]))
+                for j in T1:
+                    r1[j[4:][0][i][0]]+=1
+                for j in T2:
+                    r2[j[4:][0][i][0]]+=1
+                for j in T_total:
+                    r_total[j[4:][0][i][0]]+=1
+                N1=sum(r1)
+                N2=sum(r2)
+                N_total = sum(r_total)
+                H1 = -1.*r1/N1*np.log(r1/N1)
+                H1 = sum(np.nan_to_num(H1))
+                H2 = -1.*r2/N2*np.log(r2/N2)
+                H2 = sum(np.nan_to_num(H2))
+                H = -1.*r_total/N_total*np.log(r_total/N_total)
+                H = sum(np.nan_to_num(H))
+                JSD = H-(N1/N_total)*H1-(N2/N_total)*H2
+                all_JSD[i] = JSD
+    return all_JSD
 
