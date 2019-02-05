7eb7edd92a460b84a54c2e0b6874a474ce4d11a3
Rob Raddi <rraddi@temple.edu>
2019-02-05 17:34:20 -0500

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('./doc/source/requirements.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
         name="BICePs",
@@ -19,6 +27,6 @@ setup(
         license='MIT',
         #packages=['BIcePs'],
         packages=find_packages(),
-        install_requires=["numpy","cython","mdtraj","pymbar"],
+        install_requires=reqs,#["numpy","cython","mdtraj","pymbar"],
         include_package_data=True,
         zip_safe=False)
