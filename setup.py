e86229cce216dcf5039535ae7f6e1f2f94bcf45c
Rob Raddi <rraddi@temple.edu>
2019-02-11 19:24:54 -0500

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import sys
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
