816e320c9b5e8217c7ed7d79bd4889c4f570e4ce
Rob Raddi <rraddi@temple.edu>
2019-03-07 13:25:14 -0500

    prior test failed... Now, trying requirements1.txt (alone) first, then setup.py only including requirements2.txt

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ import sys
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 # We need two seperate requirement files due to the failure to install all at once.
-install_reqs1 = parse_requirements('./doc/requirements1.txt', session=False)
+#install_reqs1 = parse_requirements('./doc/requirements1.txt', session=False)
 
 install_reqs2 = parse_requirements('./doc/requirements2.txt', session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs1 = [str(ir.req) for ir in install_reqs1]
-req_links1 = [str(ir.url) for ir in install_reqs1]
+#reqs1 = [str(ir.req) for ir in install_reqs1]
+#req_links1 = [str(ir.url) for ir in install_reqs1]
 
 reqs2 = [str(ir.req) for ir in install_reqs2]
 req_links2 = [str(ir.url) for ir in install_reqs2]
@@ -40,8 +40,10 @@ setup(
         license='MIT',
         #packages=['BIcePs'],
         packages=find_packages(),
-        install_requires=[reqs1,reqs2],
-        dependency_links=[req_links1,req_links2],
+        #install_requires=[reqs1,reqs2],
+        install_requires=reqs2,
+        #dependency_links=[req_links1,req_links2],
+        dependency_links=req_links2,
         include_package_data=True,
         zip_safe=False)
 
