d68e64df456d61a6c76cce41d9deddd6eaa705c6
Rob Raddi <rraddi@temple.edu>
2019-03-07 12:43:16 -0500

    added session argument=False

--- setup.py
+++ setup.py
@@ -8,12 +8,19 @@ except ImportError: # for pip <= 9.0.3
 import sys
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./doc/source/requirements.txt')
+# We need two seperate requirement files due to the failure to install all at once.
+install_reqs1 = parse_requirements('./doc/source/requirements1.txt', session=False)
+
+install_reqs2 = parse_requirements('./doc/source/requirements2.txt', session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-req_links = [str(ir.url) for ir in install_reqs]
+reqs1 = [str(ir.req) for ir in install_reqs1]
+req_links1 = [str(ir.url) for ir in install_reqs1]
+
+reqs2 = [str(ir.req) for ir in install_reqs2]
+req_links2 = [str(ir.url) for ir in install_reqs2]
+
 
 
 setup(
@@ -33,8 +40,8 @@ setup(
         license='MIT',
         #packages=['BIcePs'],
         packages=find_packages(),
-        install_requires=reqs,
-        dependency_links=req_links,
+        install_requires=[reqs1,req2],
+        dependency_links=[req_links1,req_links2],
         include_package_data=True,
         zip_safe=False)
 
