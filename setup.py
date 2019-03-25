859e4ce814d56e4e6ac3f3239e3f2fe1d4fdb7dd
Rob Raddi <rraddi@temple.edu>
2019-03-25 17:25:14 -0400

    .

--- setup.py
+++ setup.py
@@ -9,25 +9,24 @@ import sys
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 # We need two seperate requirement files due to the failure to install all at once.
-#install_reqs1 = parse_requirements('./doc/requirements1.txt', session=False)
-
-install_reqs2 = parse_requirements('requirements2.txt', session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+#install_reqs1 = parse_requirements('requirements1.txt', session=False)
+#install_reqs2 = parse_requirements('requirements2.txt', session=False)
+#
+## reqs is a list of requirement
+## e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 #reqs1 = [str(ir.req) for ir in install_reqs1]
 #req_links1 = [str(ir.url) for ir in install_reqs1]
-
-reqs2 = [str(ir.req) for ir in install_reqs2]
-req_links2 = [str(ir.url) for ir in install_reqs2]
+#
+#reqs2 = [str(ir.req) for ir in install_reqs2]
+#req_links2 = [str(ir.url) for ir in install_reqs2]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
 setup(
-        name="BICePs",
-        version="2.0a",
+        name="biceps",
+        version="2.2",
         description='BICePs',
         long_description=long_description,
         long_description_content_type="text/markdown",
@@ -49,13 +48,12 @@ setup(
         author='Yunhui Ge, Robert M. Raddi, Vincent A. Voelz',
         author_email='vvoelz@gmail.com',
         license='MIT',
-        #packages=['BIcePs'],
         packages=find_packages(),
-        install_requires=reqs2,
-        #dependency_links=[req_links1,req_links2],
-        dependency_links=req_links2,
+        install_requires=[
+            'cython>=0.28','numpy>=1.7.0','pymbar','mdtraj==1.9.1'],
+        #dependency_links=req_links2,
         include_package_data=True,
-        zip_safe=False)
+        zip_safe=True)
 
 
 
