da96cf834c19d7a81eca14f00d3ecc0a59783b2d
Vincent Voelz <vvoelz@gmail.com>
2019-05-15 16:21:05 -0400

    standalone convergenge tests

---
+++ BICePs_2.0/convergence/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# make compatible with python 3
+from __future__ import print_function
+
+"""BICePs - Bayesian Inference of Conformational Populations, Version 2.0"""
+
+
---
+++ BICePs_2.0/convergence/convergence.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# make compatible with python 3
+from __future__ import print_function
+
+
