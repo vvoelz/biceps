dccf19ed141a57936ab02f48b7fe6b9dfd79ee65
yunhuige <yunhui.ge@gmail.com>
2019-07-17 14:56:00 -0400

    dataset for test

---
+++ BICePs_2.0/backup/test_new_save/cineromycin_B/biceps/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""BICePs - Bayesian Inference of Conformational Populations, Version 2.0"""
+
+#from Analysis import *
+from J_coupling import *
+from KarplusRelation import *
+from Observable import *
+from PosteriorSampler import *
+from Preparation import *
+from Restraint import *
+from init_res import *
+from prep_J import *
+from prep_cs import *
+from prep_noe import *
+from prep_pf import *
+from toolbox import *
+#from Sampler import *
+#from setup import *    # is this needed?
+from Analysis import *
+
+
