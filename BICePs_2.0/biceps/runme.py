0701ce61079630a8ac8a23a9eab510d4e41964fd
Rob Raddi <rraddi@temple.edu>
2019-08-07 12:01:47 -0400

    edits to runme.py file that will need to be changed when using the new code for PosteriorSampler

--- BICePs_2.0/biceps/runme.py
+++ BICePs_2.0/biceps/runme.py
@@ -1,28 +1,24 @@
+### In BICePs 2.0, this script will play as a center role of BICePs calucltion.
+### The users should specify all input files, type of reference potential
+### they want to use (if other than default). --Yunhui 05/2018###
+
 import sys, os, glob
 from numpy import *
-sys.path.append('src')
+sys.path.append('biceps')
 from Preparation import *
 from PosteriorSampler import *
 from Analysis import *
 from Restraint import *
-
+from init_res import *
 #########################################
 # Lets' create input files for BICePs
 ############ Preparation ################
 # Specify necessary argument values
-path='NOE/rminus*txt'
 states=100
-indices='atom_indice_noe.txt'
-exp_data='noe_distance.txt'
-top='pdb/state_00.pdb'
-data_dir=path
-dataFiles = 'test_NOE'
+top='cineromycinB_pdbs/0.fixed.pdb'
+dataFiles = 'noe_J'
 out_dir=dataFiles
 
-#p=Preparation('noe',states=states,
-#        indices=indices,exp_data=exp_data,
-#        top=top,data_dir=data_dir)
-#p.write(out_dir=out_dir)
 
 #########################################
 # Let's create our ensemble of structures
@@ -30,84 +26,48 @@ out_dir=dataFiles
 # Specify necessary argument values
 
 data = sort_data(dataFiles)
-print data
-print len(data),len(data[0])
-energies_filename =  'energy.txt'
+energies_filename =  'cineromycinB_QMenergies.dat'
 energies = loadtxt(energies_filename)
+energies = loadtxt(energies_filename)*627.509  # convert from hartrees to kcal/mol
+energies = energies/0.5959   # convert to reduced free energies F = f/kT
 energies -= energies.min()  # set ground state to zero, just in case
-outdir = 'results_ref_normal_TEST_NOE'
-nsteps = 1000000 # 10000000
+outdir = 'results_ref_normal'
+nsteps = 1000000
 
 # Make a new directory if we have to
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
+uncern=[[0.05,20.0,1.02],[0.05,5.0,1.02]]
+gamma = [0.2,5.0,1.01]
+
+ref=['uniform','exp']
+
 ######################
 # Main:
 ######################
-lambda_values = [0.0,1.0]
+
+lambda_values = [0.0,0.5,1.0]
+
 for j in lambda_values:
-    verbose = True#False
+    print 'lambda', j
+    verbose = False #False
     lam = j
-    # We will instantiate a number of Structure() objects to construct the ensemble
+    # We will instantiate a number of Restraint() objects to construct the ensemble
+    # experimental data and pre-computed model data are compiled for each state
     ensemble = []
-    for i in range(energies.shape[0]):
-        print '\n#### STRUCTURE %d ####'%i
+    for i in range(energies.shape[0]):   # number of states
+        if verbose:
+            print '\n#### STRUCTURE %d ####'%i
         ensemble.append([])
-        for k in range(len(data[0])):
+        for k in range(len(data[0])):   # number of experimental observables
             File = data[i][k]
             if verbose:
                 print File
-
-            # Call on the Restraint that corresponds to File
-            if File.endswith('cs_H'):
-                R = Restraint_cs_H('pdb/state_%02d.pdb'%i,ref='exp')
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-                # Change the experimental Uncertainty after prepping observable
-                R.exp_uncertainty(dlogsigma=np.log(1.02),sigma_min=0.05,
-                        sigma_max=20.0)
-
-            elif File.endswith('cs_CA'):
-                R = Restraint_cs_Ca('pdb/state_%02d.pdb'%i,ref='gaussian')
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-
-            elif File.endswith('cs_Ha'):
-                R = Restraint_cs_Ha('pdb/state_%02d.pdb'%i,ref='exp')
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-                # Change the experimental Uncertainty after prepping observable
-                R.exp_uncertainty(dlogsigma=np.log(1.02),sigma_min=10,
-                        sigma_max=20.0)
-
-            elif File.endswith('cs_N'):
-                R = Restraint_cs_N('pdb/state_%02d.pdb'%i,ref='gaussian')
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-
-            elif File.endswith('J'):
-                R = Restraint_J('pdb/state_%02d.pdb'%i,ref='uniform')  # good ref
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-                R.exp_uncertainty(dlogsigma=np.log(1.02),sigma_min=5,
-                        sigma_max=20.0)
-
-            elif File.endswith('noe'):
-                R = Restraint_noe('pdb/state_%02d.pdb'%i,ref='exp')   # good ref
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-                # Change the experimental Uncertainty after prepping observable
-                R.exp_uncertainty(dlogsigma=np.log(1.01),sigma_min=0.2,
-                        sigma_max=10.0)
-
-            elif File.endswith('pf'):
-                R = Restraint_pf('8690.pdb',ref='gaussian')
-                R.prep_observable(lam=lam, free_energy=energies[i],
-                        filename=File)
-
+            R=init_res('cineromycinB_pdbs/0.fixed.pdb',lam,energies[i],File,ref[k],uncern=uncern[k],gamma=gamma)
             ensemble[-1].append(R)
-    print ensemble
+
+#    print ensemble
 
     ##########################################
     # Next, let's do some posterior sampling
@@ -120,13 +80,12 @@ for j in lambda_values:
 
     print 'Processing trajectory...',
 
-    sampler.traj.process()  # compute averages, etc.
+    #NOTE: process function is now combined with write results
+    sampler.traj.process_results(os.path.join(outdir,'RR_traj_lambda%2.2f.npz'%lam))  # compute averages, etc.
     print '...Done.'
 
-    print 'Writing results...',
-    sampler.traj.write_results(os.path.join(outdir,'traj_lambda%2.2f.npz'%lam))
-    print '...Done.'
-    sampler.traj.read_results(os.path.join(outdir,'traj_lambda%2.2f.npz'%lam))
+    sampler.traj.read_results(os.path.join(outdir,'RR_traj_lambda%2.2f.npz'%lam))
+    #sampler.traj.read_results(os.path.join(outdir,'traj_lambda%2.2f.npz'%lam))
 
     print 'Pickling the sampler object ...',
     outfilename = 'sampler_lambda%2.2f.pkl'%lam
@@ -138,13 +97,12 @@ for j in lambda_values:
     print '...Done.'
 
 
-sys.exit(1)
 
 #########################################
 # Let's do analysis using MBAR and plot figures
 ############ MBAR and Figures ###########
 # Specify necessary argument values
-
-A = Analysis(50,dataFiles,outdir)
+exit(1)
+A = Analysis(100,outdir)
 A.plot()
 
