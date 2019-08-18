f201f8dca911e9e3d5a5e3d754cf2a906cfa5421
Rob Raddi <rraddi@temple.edu>
2019-08-18 11:51:30 -0400

    adding cython decorator that allows the use of docstrings -- @cython.embedsiignature(True) ...otherwise, you get an error upon compile

--- BICePs_2.0/biceps/c_convergence.pyx
+++ BICePs_2.0/biceps/c_convergence.pyx
@@ -9,8 +9,16 @@ cdef extern from "convergence.h":
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
+@cython.embedsignature(True)
 def autocorrelation(vector[vector[float]] sampled_parameters,
         int maxtau=10000, bool normalize=True):
+    """Calculate the autocorrelaton function for a time-series f(t).
+
+    :param np.array sampled_parameters: a 2D numpy array containing the time series f(t) for each nuisance parameterurn c_autocorrelation(sampled_parameters, maxtau, normalize)
+    :param int max_tau: the maximum autocorrelation time to consider.
+    :param bool normalize: if True, return g(tau)/g[0]
+    :return np.array: a numpy array of size (3, max_tau+1) containing g(tau) for each nuisance parameter
+    """
 
     return c_autocorrelation(sampled_parameters, maxtau, normalize)
 
