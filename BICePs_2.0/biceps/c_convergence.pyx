e0db52b9bffdcae40ea86646a7519b91b59c64fa
Rob Raddi <rraddi@temple.edu>
2019-08-19 17:15:15 -0400

    mistake in docstring fixed.

--- BICePs_2.0/biceps/c_convergence.pyx
+++ BICePs_2.0/biceps/c_convergence.pyx
@@ -7,9 +7,9 @@ cdef extern from "convergence.h":
             vector[vector[float]] sampled_parameters,
             int maxtau, bool normalize)
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.embedsignature(True)
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+#@cython.embedsignature(True)
 def autocorrelation(vector[vector[float]] sampled_parameters,
         int maxtau=10000, bool normalize=True):
     """Calculate the autocorrelaton function for a time-series f(t).
@@ -17,7 +17,7 @@ def autocorrelation(vector[vector[float]] sampled_parameters,
     :param np.array sampled_parameters: a 2D numpy array containing the time series f(t) for each nuisance parameterurn c_autocorrelation(sampled_parameters, maxtau, normalize)
     :param int max_tau: the maximum autocorrelation time to consider.
     :param bool normalize: if True, return g(tau)/g[0]
-    :return np.array: a numpy array of size (3, max_tau+1) containing g(tau) for each nuisance parameter
+    :return np.array: array of size (len(sampled_parameters), max_tau+1) containing g(tau) for each nuisance parameter
     """
 
     return c_autocorrelation(sampled_parameters, maxtau, normalize)
