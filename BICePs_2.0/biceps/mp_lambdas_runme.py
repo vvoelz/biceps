9272a29bab159499c2e037c30357b71ba674ebcc
Rob Raddi <rraddi@temple.edu>
2019-11-14 14:53:49 -0500

    adding runme file for parallel lambda values

---
+++ BICePs_2.0/biceps/mp_lambdas_runme.py
@@ -0,0 +1,78 @@
+import os, cPickle
+import sys
+import numpy as np
+sys.path.append("../")
+import biceps
+import multiprocessing as mp
+####### Data and Output Directories #######
+energies = np.loadtxt('cineromycinB_QMenergies.dat')
+data = biceps.sort_data('noe_J')
+res = biceps.list_res(data)
+outdir = 'results_ref_normal'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+####### Parameters #######
+lambda_values = [0.0, 0.5, 1.0]
+ref = ['uniform', 'exp']
+uncern = [[0.05, 20.0, 1.02], [0.05, 5.0, 1.02]]
+#############################################
+####### Multiprocessing Lambda values #######
+#############################################
+def mp_lambdas(Lambda):
+    ####### MCMC Simulations #######
+    ensemble = []
+    for i in range(energies.shape[0]):
+        ensemble.append([])
+        for k in range(len(data[0])):
+            File = data[i][k]
+            R = biceps.init_res(PDB_filename='cineromycinB_pdbs/0.fixed.pdb', lam=lam,
+                energy=energies[i], ref=ref[k], data=File,
+                uncern=uncern[k], gamma=[0.2, 5.0, 1.02])
+            ensemble[-1].append(R)
+    sampler = biceps.PosteriorSampler(ensemble)
+    sampler.sample(nsteps=100000)
+    sampler.traj.process_results(outdir+'/traj_lambda%2.2f.npz'%(lam))
+    sampler.traj.read_results(os.path.join(outdir,
+        'traj_lambda%2.2f.npz'%lam))
+    outfilename = 'sampler_lambda%2.2f.pkl'%(lam)
+    fout = open(os.path.join(outdir, outfilename), 'wb')
+    cPickle.dump(sampler, fout)
+    fout.close()
+    print('...Done.')
+# Check the number of CPU's available
+print("Number of CPU's: %s"%(mp.cpu_count()))
+p = mp.Pool(processes=mp.cpu_count()-1) # knows the number of CPU's to allocate
+#print("Process ID's: %s"%get_processes(p, n=lam))
+jobs = []
+for lam in lambda_values:
+    process = p.Process(target=mp_lambdas, args=(lam,))
+    jobs.append(process)
+    jobs[-1].start() # Start the processes
+    active_processors = [jobs[i].is_alive() for i in range(len(jobs))]
+    print("Active Processors: %s"%active_processors)
+    if (len(active_processors) == mp.cpu_count()-1) and all(active_processors) == True:
+        print("Waiting until a processor becomes available...")
+        while all(active_processors) == True:
+            active_processors = [jobs[i].is_alive() for i in range(len(jobs))]
+        print(active_processors)
+        inactive = int(np.where(np.array(active_processors) == False)[0])
+        jobs[inactive].terminate()
+        jobs.remove(jobs[inactive])
+for job in jobs:
+    job.join() # will wait until the execution is over...
+p.close()
+####### Posterior Analysis #######
+A = biceps.Analysis(states=100, resultdir=outdir,
+    BSdir=outdir+'/BS.dat', popdir=outdir+'/populations.dat',
+    picfile=outdir+'/BICePs.pdf')
+A.plot()
+######## Convergence #######
+#C = biceps.Convergence(trajfile='traj_lambda1.00.npz',
+#    maxtau=10000, nblock=5, nfold=10, nround=1000)
+#C.get_autocorrelation_curves(method="block-avg", nblocks=5,
+#    plot_traces=True)
+#C.process(nblock=5, nfold=10, nrounds=100, savefile=True,
+#    plot=True, block=False, normalize=True)
+
+
+
