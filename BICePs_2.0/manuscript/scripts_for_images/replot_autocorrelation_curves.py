46e6fe9ece408fa23b987876c7c68d42387cc88e
Rob Raddi <rraddi@temple.edu>
2020-01-07 17:25:14 -0500

    minor update to reflect src

--- BICePs_2.0/manuscript/scripts_for_images/replot_autocorrelation_curves.py
+++ BICePs_2.0/manuscript/scripts_for_images/replot_autocorrelation_curves.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     # User specified paths
     traj = "/Volumes/WD_Passport_1TB/new_sampling/new_sampling/d_1.01/results_ref_normal_1000000/traj_lambda1.00.npz"
     #traj = "/Volumes/WD_Passport_1TB/new_sampling/new_sampling/d_1.01/results_ref_normal_100000/traj_lambda1.00.npz"
-    #C = biceps.Convergence(trajfile=traj, maxtau=1000)
-    C = biceps.Convergence(trajfile=traj, maxtau=5000)
-    C.get_autocorrelation_curves(method="normal")
+    #C = biceps.Convergence(trajfile=traj)
+    C = biceps.Convergence(trajfile=traj)
+    C.get_autocorrelation_curves(method="normal", maxtau=5000)
     C.process(nblock=5, nfold=10, nround=100, savefile=True,
         plot=True, block=False, normalize=True)
-    C.get_autocorrelation_curves(method="exp")
+    C.get_autocorrelation_curves(method="exp", maxtau=5000)
 
 
 
