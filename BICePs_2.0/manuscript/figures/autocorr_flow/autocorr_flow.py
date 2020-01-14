7cda8a4e42396779edc0313a600c1a648487e407
Rob Raddi <rraddi@temple.edu>
2020-01-14 18:07:39 -0500

    updated autocorr_flow figure (now using gamma)

--- BICePs_2.0/manuscript/figures/autocorr_flow/autocorr_flow.py
+++ BICePs_2.0/manuscript/figures/autocorr_flow/autocorr_flow.py
@@ -2,8 +2,8 @@ import sys,os
 sys.path.append("../../../")
 import biceps
 
-C = biceps.Convergence(trajfile="traj_lambda1.00.npz")
-C.get_autocorrelation_curves(method="normal", maxtau=5000)
+C = biceps.Convergence(trajfile="./traj_lambda1.00.npz")
+C.get_autocorrelation_curves(method="normal", maxtau=50000)
 C.process(nblock=5, nfold=10, nround=100, savefile=True,
     plot=True, block=True, normalize=True)
 
