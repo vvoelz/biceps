46e83b25f408dfc15bcedf40ec4011a4ca36344b
Rob Raddi <rraddi@temple.edu>
2020-05-13 19:46:35 -0400

    adding docorators.py and updated tutorial

---
+++ biceps/decorators.py
@@ -0,0 +1,32 @@
+import multiprocessing as mp
+import numpy as np
+
+def multiprocess(*args, **kwargs):
+    """Decorator method for multiprocessing an embarassingly parallel function.
+    """
+
+    def wrapper(function):
+        n = len(kwargs['iterator'])
+        print("Number of CPU's: %s"%(mp.cpu_count()))
+        p = mp.Pool(processes=n)
+        print(f"Number of processes: {n}")
+        jobs = []
+        for iter in kwargs['iterator']:
+            process = p.Process(target=function, args=(iter,))
+            jobs.append(process)
+            jobs[-1].start()
+            active_processors = [jobs[i].is_alive() for i in range(len(jobs))]
+            if (len(active_processors) == mp.cpu_count()-1) and all(active_processors) == True:
+                while all(active_processors) == True:
+                    active_processors = [jobs[i].is_alive() for i in range(len(jobs))]
+                inactive = int(np.where(np.array(active_processors) == False)[0])
+                jobs[inactive].terminate()
+                jobs.remove(jobs[inactive])
+        for job in jobs:
+            job.join()
+        p.close()
+    return wrapper
+
+
+
+
